36c0839bf1196b62f46a1fa0829e3572f247601f
Theodore Thayib <tpthayib@iastate.edu>
2017-10-20 10:06:43 -0500

    Test Commit

--- app/src/main/java/com/example/theo1/hackisu17f/MainActivity.java
+++ app/src/main/java/com/example/theo1/hackisu17f/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Toast.makeText(getApplicationContext(), "Test", Toast.LENGTH_LONG).show();
     }
 
     @Override
